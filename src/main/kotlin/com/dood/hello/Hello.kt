ca7b7d054dfdb696d5fc0a4e7b0a11ba9f7114fe
Randy Watson <drbugsmn@gmail.com>
2022-05-11 21:24:33 -0500

    Added logging, and a post endpoint to experiment with json requests and responses

--- src/main/kotlin/com/dood/hello/Hello.kt
+++ src/main/kotlin/com/dood/hello/Hello.kt
@@ -1,21 +1,33 @@
 package com.dood.hello
 
+import io.micronaut.core.annotation.Introspected
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
 import io.micronaut.serde.annotation.Serdeable
+import org.slf4j.LoggerFactory
 import java.time.Instant
 
 @Controller("/hello")
 class HelloController {
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     @Get(produces = [MediaType.APPLICATION_JSON]) //
     fun index(): HelloResponse {
         return HelloResponse("Hello World", Instant.now())//
     }
+
+    @Post(produces = [MediaType.APPLICATION_JSON])
+    fun create(request: HelloRequest): HelloResponse {
+        logger.info("received an input: $request")
+        return HelloResponse("${request.salutation} ${request.name}!", Instant.now())
+    }
 }
 
 //TODO add validation to request
+@Introspected
+@Serdeable //how to do jackson?
 data class HelloRequest(val salutation: String, val name: String)
 
 @Serdeable
--- src/test/kotlin/com/dood/hello/HelloControllerSpec.kt
+++ src/test/kotlin/com/dood/hello/HelloControllerSpec.kt
@@ -5,6 +5,8 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.date.shouldBeBefore
 import io.kotest.matchers.shouldBe
 import io.micronaut.http.HttpRequest.GET
+import io.micronaut.http.HttpRequest.POST
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.kotest.annotation.MicronautTest
@@ -24,6 +26,27 @@ class HelloControllerSpec(@Client("/") private val client: HttpClient) : FunSpec
                 .retrieve(GET<Any>("/hello"), HelloResponse::class.java)
         rsp.response shouldBe "Hello World"
         rsp.createTime shouldBeBefore Instant.now()
+    }
+    test("POST /hello") {
+        val request = HelloRequest("Hello", "dood")
+        val rsp = client.toBlocking().exchange( //toblocking isnt prefered vs subscribe (reactive)
+            POST("/hello", request), HelloResponse::class.java
+        )
+        rsp.status shouldBe HttpStatus.OK
+        rsp.body() != null //let block?
+        val body = rsp.body.get()
+        //kotest and optionals https://www.novatec-gmbh.de/blog/kotlin-assertion-libraries-kotest-assertions/
+        body.response.shouldBe("Hello dood!")
+        body.createTime.shouldBeBefore(Instant.now())
+    }
+
+    test("POST /hello with a HttpClient retreive") {
+        val request = HelloRequest("Hello", "dood")
+        val rsp =
+            client.toBlocking().retrieve(POST("/hello", request)) //toblocking isnt prefered vs subscribe (reactive)
+        //retreive only givess us the body, not the headers and status and such
+//response contains the JSON string of HelloResponse
+        rsp.contains("Hello dood!")
     }
 })
 
